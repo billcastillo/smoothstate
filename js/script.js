492cb600ca4ff5d5930b99256c8e53199db43579
Bill <castillobil@gmail.com>
2016-06-11 23:35:30 +0400

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-$(function() {
+$(function(){
   'use strict';
-  $('#main').smoothState({ debug: true });
+  var $page = $('#main'),
+      options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 4,
+        onStart: {
+          duration: 350, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+            // Inject the new content
+            $container.html($newContent);
+          }
+        }
+      },
+      smoothState = $page.smoothState(options).data('smoothState');
 });
